c89f3ca781b7900efba336b926d515253b18e5e8
Kenny Chen <kennychen851228@gmail.com>
2021-04-24 16:40:34 +0800

    feat: Implement Postgres related store way

--- stripeapi/cmd/main.go
+++ stripeapi/cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/KennyChenFight/golib/loglib"
+	"github.com/KennyChenFight/golib/migrationlib"
+	"github.com/KennyChenFight/golib/pglib"
 	"github.com/KennyChenFight/golib/uuidlib"
 	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg/v10"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/jessevdk/go-flags"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
 	"os"
-	"sync"
 )
+
+type PostgresConfig struct {
+	URL       string `long:"url" description:"the url of postgres" env:"URL" required:"true"`
+	PoolSize  int    `long:"pool-size" description:"the poolSize of postgres" env:"POOL_SIZE" default:"5"`
+	DebugMode bool   `long:"debug-mode" description:"the debugMode of postgres" env:"DEBUG_MODE"`
+}
+
+type MigrationConfig struct {
+	SourceURL string `long:"source-url" description:"the source url of file" env:"SOURCE_URL" default:"file://stripeapi/migrations"`
+}
 
 type GinConfig struct {
 	Port string `long:"port" description:"port" env:"PORT" default:"8081"`
@@ -18,7 +33,9 @@ type GinConfig struct {
 }
 
 type Environment struct {
-	GinConfig GinConfig `group:"gin" namespace:"gin" env-namespace:"GIN"`
+	GinConfig       GinConfig       `group:"gin" namespace:"gin" env-namespace:"GIN"`
+	PostgresConfig  PostgresConfig  `group:"postgres" namespace:"postgres" env-namespace:"POSTGRES"`
+	MigrationConfig MigrationConfig `group:"migration" namespace:"migration" env-namespace:"MIGRATION"`
 }
 
 type Charge struct {
@@ -28,7 +45,8 @@ type Charge struct {
 	Capture bool   `json:"capture"`
 }
 
-var MemoryStore = sync.Map{}
+var ErrChargeNotFound = errors.New("charge not found")
+var ErrChargeConflict = errors.New("already charge this user")
 
 func main() {
 	var env Environment
@@ -41,6 +59,26 @@ func main() {
 			log.Println("parse env error:", err)
 			os.Exit(1)
 		}
+	}
+
+	err := migrationlib.NewMigrateLib(migrationlib.Config{
+		DatabaseDriver: migrationlib.PostgresDriver,
+		DatabaseURL:    env.PostgresConfig.URL,
+		SourceDriver:   migrationlib.FileDriver,
+		SourceURL:      env.MigrationConfig.SourceURL,
+		TableName:      "migration_versions",
+	}).Up()
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("fail to migrate:%v", err)
+	}
+
+	pgClient, err := pglib.NewDefaultGOPGClient(pglib.GOPGConfig{
+		URL:       env.PostgresConfig.URL,
+		DebugMode: env.PostgresConfig.DebugMode,
+		PoolSize:  env.PostgresConfig.PoolSize,
+	})
+	if err != nil {
+		log.Fatalf("fail to connect to postgres:%v", err)
 	}
 
 	logger, err := loglib.NewProductionLogger()
@@ -60,26 +98,47 @@ func main() {
 			charge.ID = uuidlib.NewV4().String()
 			charge.Capture = false
 			// do not charge, just store charge
-			MemoryStore.Store(charge.ID, charge)
+			_, err := pgClient.Model(&charge).Insert()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+				return
+			}
 			logger.Info("charges", zap.Any("charge", charge))
 			c.JSON(http.StatusCreated, gin.H{"id": charge.ID})
 		})
 
 		v1Group.PATCH("/charges/:id", func(c *gin.Context) {
 			id := c.Param("id")
-			obj, ok := MemoryStore.Load(id)
-			if ok {
-				charge := obj.(Charge)
+			charge := Charge{ID: id}
+			err := pgClient.RunInTransaction(context.Background(), func(tx *pg.Tx) error {
+				err := tx.Model(&charge).WherePK().For("UPDATE").Select()
+				if err != nil {
+					return err
+				}
 				if !charge.Capture {
 					charge.Capture = true
-					MemoryStore.Store(id, charge)
-					logger.Info("charges", zap.Any("charge", charge))
-					c.JSON(http.StatusNoContent, nil)
+					_, err = tx.Model(&charge).Set("capture = ?capture").WherePK().Update()
+					if err != nil {
+						return err
+					}
 				} else {
-					c.JSON(http.StatusConflict, gin.H{"message": "already charge this user"})
+					return ErrChargeConflict
 				}
+				return nil
+			})
+			if err != nil {
+				if err == pg.ErrNoRows {
+					c.JSON(http.StatusNotFound, ErrChargeNotFound)
+					return
+				}
+				if err == ErrChargeConflict {
+					c.JSON(http.StatusConflict, ErrChargeConflict)
+					return
+				}
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 			} else {
-				c.JSON(http.StatusNotFound, gin.H{"message": "can not find charge"})
+				logger.Info("charges", zap.Any("charge", charge))
+				c.JSON(http.StatusNoContent, nil)
 			}
 		})
 	}
